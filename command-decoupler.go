7be75dd17658512f765987739a796cbbf71f11e0
Rob Petti <rob.petti@gmail.com>
2018-02-28 23:14:27 -0700

    handle connections in a go routine

--- command-decoupler.go
+++ command-decoupler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
 	"time"
 
@@ -13,6 +14,18 @@ var (
 	pipe = "\\\\.\\pipe\\command-decoupler"
 )
 
+func connectionHandler(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Printf("s: Message Received: %s", message)
+	}
+	fmt.Println("s: Connection closed.")
+}
+
 func pipeServer() {
 	listener, err := winio.ListenPipe(pipe, &winio.PipeConfig{})
 	if err != nil {
@@ -20,42 +33,35 @@ func pipeServer() {
 		os.Exit(1)
 	}
 	for {
-		fmt.Println("Listening for connections...")
+		fmt.Println("s: Listening for connections...")
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("Error accepting connection.")
+			fmt.Printf("s: Error accepting connection.")
 			break
 		}
-		fmt.Println("Connection accepted. Reading data...")
-		reader := bufio.NewReader(conn)
-		for {
-			message, err := reader.ReadString('\n')
-			if err != nil {
-				break
-			}
-			fmt.Printf("message: %s", message)
-		}
+		fmt.Println("s: Connection accepted.")
+		go connectionHandler(conn)
 	}
-	fmt.Println("Done serving. Closing pipe listener.")
+	fmt.Println("s: Done serving. Closing pipe listener.")
 	listener.Close()
 }
 
 func main() {
 	go pipeServer()
 	time.Sleep(1 * time.Second)
-	fmt.Println("Connecting to pipe...")
+	fmt.Println("c: Connecting to pipe...")
 	timeout := 5 * time.Second
 	conn, err := winio.DialPipe(pipe, &timeout)
 	if err != nil {
-		fmt.Printf("Could not connect to to pipe: %s\n", err)
+		fmt.Printf("c: Could not connect to to pipe: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Println("Connected. Writing data...")
+	fmt.Println("c: Connected. Writing data...")
 	writer := bufio.NewWriter(conn)
 	writer.WriteString("This is a test message.\n")
 	writer.Flush()
-	fmt.Println("Data written. Closing pipe...")
+	fmt.Println("c: Data written. Closing pipe...")
 	conn.Close()
-	fmt.Println("Done.")
+	fmt.Println("c: Done.")
 	time.Sleep(1 * time.Second)
 }
